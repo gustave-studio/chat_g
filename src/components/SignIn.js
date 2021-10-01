2810bd2e7901776ef2a6aace6934ac3725a494e0
gustave-studio <gustave0222@gmail.com>
2021-10-02 00:39:14 +0900

    ニックネームが空の場合は、ボタンを押せないようにする

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -22,6 +22,15 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn({ setName }) {
+  const classes = useState();
+  const [disabled, setDisabled] = useState(true);
+  const [string, setString] = useState('');
+  console.log(string);
+  useEffect(() => {
+    const disabled = string === ''
+    setDisabled(disabled)
+  }, [string]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -57,12 +66,14 @@ export default function SignIn({ setName }) {
               name="name"
               autoComplete="email"
               autoFocus
+              onChange={(e) => setString(e.target.value)}
             />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={disabled}
             >
               はじめる
             </Button>
